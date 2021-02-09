3ee9d949edd570206fe18579f75c07abea5c6740
Borlone <borlone196@gmail.com>
2021-02-09 16:28:44 +0700

    update API for upload

--- routes/File.js
+++ routes/File.js
@@ -3,16 +3,14 @@ const { uploadAvatar, uploadFiles } = require('../middleware/upload');
 
 const router = express.Router();
 
-const hostname = process.env.PORT || 'localhost:5000';
-
 // upload image for avatar
 router.post('/upload/avatar', uploadAvatar.single('avatar'), (req, res) => {
     const file = req.file;
     if (!file) {
         return res.status(400).json('Please upload a file.')
     }
-    const avatarPath = `${req.protocol}://${hostname}/${file.path}`
-    res.json({ avatar: avatarPath })
+    const hostname = process.env.PORT ? req.hostname : `${req.hostname}:5000`
+    res.json({ avatar: `${req.protocol}://${hostname}/${file.path}` })
 })
 
 // upload files for attachment
